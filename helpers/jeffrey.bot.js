564a46b03cffdd37f04b48545f66e55b7f2f1dfc
lihbr <lihbr@users.noreply.github.com>
2019-03-24 23:24:36 +0100

    :wrench: fix bearer

--- helpers/jeffrey.bot.js
+++ helpers/jeffrey.bot.js
@@ -3,7 +3,7 @@
  */
 const axios = require("axios");
 
-axios.defaults.headers.common["Authorization"] = `bearer ${
+axios.defaults.headers.common["Authorization"] = `Bearer ${
   process.env.SLACK_BOT_OAUTH_TOKEN
 }`;
 
